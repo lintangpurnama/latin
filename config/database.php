1ac85b57c0c5d1c1258eb884fb8eca45b2c1d0d8
unknown <lintangpurnama1@gmail.com>
2026-06-15 05:48:39 +0700

    update db update

--- config/database.php
+++ config/database.php
@@ -59,7 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => '/path/to/ca-cert.pem',
+                'mysql_attr_ssl_ca' => '/path/to/ca-cert.pem',
+
             ]) : [],
         ],
 
